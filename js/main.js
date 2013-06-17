8d0d70e527a255bb6d4ae35571d5ab11ae0922be
Andrey <andrew.samoilov@gmail.com>
2013-06-17 12:41:59 +0400

    refactoring

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+	var wh = $(window).height();
+	
+	$('section.slider').height(wh);
+	
+	$('#map-canvas').height(wh - 140);
+	initialize();
+	
+	$('.__fotorama').fotorama({
+		nav: 'dots', 
+		transition: 'fade',
+		autoplay: 'false',
+		margin: 0,
+		minPadding: 0,
+		loop: true,
+		cropToFit: true,
+		width: '100%',
+		height: wh,
+		arrowNext: '<div class="arrows right_arrow"></div>',
+		arrowPrev: '<div class="arrows left_arrow"></div>'
+	});
+
+});
+
