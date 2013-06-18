3eb987525381daaff6c3c60b3982faf0cecb7951
Tumin Artem <at@grapheme.ru>
2013-06-18 19:16:19 +0400

    LAST version

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 var st = st || {};
 
 
-
-
 st.init = function() {
 	st.wh = $(window).height();
 	
@@ -61,32 +59,6 @@ $(function(){
 	st.init();
 
 	var wh = $(window).height();	
-
-	
-	/*
-	var newsHeight = $('#news').height();
-	var restHeight = $('#restaurant-info').height();
-	var poolHeight = $('#pool-info').height();
-	console.log(restHeight);
-	
-	$('#news').attr('data-2000', 'top: 0px;');
-	$('#news').attr('data-4800', 'top: -' + newsHeight + 'px;');
-	
-	$('#restaurant-info').attr('data-6400', 'top: 0px;');
-	$('#restaurant-info').attr('data-8000', 'top: -' + restHeight + 'px;');
-	
-	$('#pool-info').attr('data-9600', 'top: 0px;');
-	$('#pool-info').attr('data-10800', 'top: -' + poolHeight + 'px;');
-	
-	console.log($('#restaurant-info').attr('data-6400'));
-	console.log($('#restaurant-info').attr('data-8000'));
-	/*$('#restaurant-info').attr('data-8000', '-120%;');
-	console.log($('#restaurant-info').attr('data-8000'));
-	/*$('#restaurant-info').attr({
-		'data-6400' : 'top:0px;',
-		'data-8000' : '-' + (rastheight) + 'px;'
-	});*/
-	
 	
 	$('#map-canvas').height(wh - 140);
 	initialize();
@@ -121,7 +93,7 @@ $(function(){
 
 $(window).resize( function() {	
 	st.init();
-	
+	var s = skrollr.init({ });
 	$(function() {
 		
     $('.__fotorama').fotorama({height: st.wh});
