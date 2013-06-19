783f4ee0f340b7513a1f4154be5add92bcc4de82
Vladimir Kharseev <vkharseev@gmail.com>
2013-06-19 11:58:26 +0400

    19.06.13 12:00

--- application/controllers/guests_interface.php
+++ application/controllers/guests_interface.php
@@ -22,4 +22,14 @@ class Guests_interface extends MY_Controller{
 		$this->load->view("guests_interface/accounts/signin");
 	}
 	
+	public function logoff(){
+		
+		$this->clearSession(FALSE);
+		$this->session->unset_userdata(array('logon'=>'','profile'=>'','account'=>'','backpath'=>''));
+		if(isset($_SERVER['HTTP_REFERER'])):
+			redirect($_SERVER['HTTP_REFERER']);
+		else:
+			redirect('');
+		endif;
+	}
 }
