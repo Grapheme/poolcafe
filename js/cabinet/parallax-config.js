3a5d4074371974c813eb74ba712ae3f6607728f3
Tumin Artem <at@grapheme.ru>
2013-06-25 17:47:57 +0400

    menu font-size = aquarium font-size

--- js/cabinet/parallax-config.js
+++ js/cabinet/parallax-config.js
@@ -2,6 +2,5 @@
  *  http://grapheme.ru/
  */
 $(function(){
-	$('div.div-octopus img.parallax-layer').parallax({},{xparallax:'100px',decay: 0.99},{xparallax:'100px',decay:0.66 });
-	$('div.grandies img.parallax-layer').parallax({},{xparallax:'100px',decay:0.99},{xparallax:'100px',decay:0.66});
+	$('img.parallax-layer').parallax({},{xparallax:'100px',decay: 0.99},{xparallax:'100px',decay:0.66 });
 });
