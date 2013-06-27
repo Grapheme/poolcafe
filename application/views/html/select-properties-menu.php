dacf93a1e2edc1fd79512fb05ab310fd55b362ac
Vladimir Kharseev <vkharseev@gmail.com>
2013-06-27 13:21:32 +0400

    fix text

--- application/views/html/select-properties-menu.php
+++ application/views/html/select-properties-menu.php
@@ -5,7 +5,7 @@
 	<?php endfor;?>
 	</select>
 	<select autocomplete="off" class="select-parents-categories" name="parents">
-		<option value="">Выбирите категорию</option>
+		<option value="">Выберите категорию</option>
 	<?php for($i=0;$i<count($categories);$i++):?>
 		<?php if($categories[$i]['parent'] == 0):?>
 		<option value="<?=$categories[$i]['id'];?>" <?=($this->input->get('category') == $categories[$i]['id'])?'selected="selected"':'';?>><?=$categories[$i]['title'];?></option>
@@ -14,7 +14,7 @@
 	</select>
 <?php if($this->input->get('category') !== FALSE && isset($categories[$this->input->get('category')]['children'])):?>
 	<select autocomplete="off" class="select-subcategories" name="parents">
-		<option value="">Выбирите подкатегорию</option>
+		<option value="">Выберите подкатегорию</option>
 	<?php for($i=0;$i<count($categories[$this->input->get('category')]['children']);$i++):?>
 		<?php if($categories[$this->input->get('category')]['children'][$i]['parent'] == $this->input->get('category')):?>
 		<option value="<?=$categories[$this->input->get('category')]['children'][$i]['id'];?>" <?=($this->input->get('subcategory') == $categories[$this->input->get('category')]['children'][$i]['id'])?'selected="selected"':'';?>><?=$categories[$this->input->get('category')]['children'][$i]['title'];?></option>
