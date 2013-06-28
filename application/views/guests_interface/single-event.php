fc6e6ccb703c18781a8981e448264ad486d13a13
Tumin Artem <at@grapheme.ru>
2013-06-28 14:14:38 +0400

    pipki added

--- application/views/guests_interface/single-event.php
+++ application/views/guests_interface/single-event.php
@@ -24,6 +24,20 @@
 				<div class="sep_new-date">
 					<?=monthDateSpan($events['date_publish'],'month-name');?>
 					<br/><?=$events['category_title'];?>
+					<div class="back-forward-links">
+						<ul class="back-forward-links-list">
+							<li class="back-forward-links-list-item back">
+								<a href="#"><img src="<?=base_url('img/linkback.png');?>"></a>
+							</li>
+							<li class="back-forward-links-list-item mainmenu">
+								<a href="#"><img src="<?=base_url('img/mainmenu.png');?>"></a>
+							</li>
+							<li class="back-forward-links-list-item forward">
+								<a href="#"><img src="<?=base_url('img/linkforward.png');?>"></a>
+							</li>
+						</ul>	
+
+					</div><div class="clear"></div>
 				</div>
 				<div class="sep_new-header">
 					<h1 class="news-item-header"><?=$events['title'];?></h1>
