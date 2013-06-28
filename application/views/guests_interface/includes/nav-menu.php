088d77d9c346376ed5abc7860a047730c72b1c49
Vladimir Kharseev <vkharseev@gmail.com>
2013-06-28 14:58:14 +0400

    add current-link class

--- application/views/guests_interface/includes/nav-menu.php
+++ application/views/guests_interface/includes/nav-menu.php
@@ -5,13 +5,13 @@
 <?php endif;?>
 	<ul class="menu-nav-list">
 		<li class="menu-nav-list-item item1">
-			<a href="<?=site_url('menu');?>"> <div></div> <span>Меню</span> </a>
+			<a <?=($this->uri->segment(1) == 'menu')?'class="current-link"':'';?> href="<?=site_url('menu');?>"> <div></div> <span>Меню</span> </a>
 		</li>
 		<li class="menu-nav-list-item item2">
-			<a href="<?=site_url('wine-card');?>"> <div></div> <span>Винная карта</span> </a>
+			<a <?=($this->uri->segment(1) == 'wine-card')?'class="current-link"':'';?> href="<?=site_url('wine-card');?>"> <div></div> <span>Винная карта</span> </a>
 		</li>
 		<li class="menu-nav-list-item item4">
-			<a href="<?=site_url('aquarium');?>"> <div></div> <span>Аквариум</span> </a>
+			<a <?=($this->uri->segment(1) == 'aquarium')?'class="current-link"':'';?> href="<?=site_url('aquarium');?>"> <div></div> <span>Аквариум</span> </a>
 		</li>
 	</ul>
 </div>
