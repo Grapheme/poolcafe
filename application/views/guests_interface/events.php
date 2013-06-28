a6682b6de162d8224a4606cabe51e2bbee48b679
Tumin Artem <at@grapheme.ru>
2013-06-28 14:34:43 +0400

    color-logo => white-logo

--- application/views/guests_interface/events.php
+++ application/views/guests_interface/events.php
@@ -13,7 +13,7 @@
 	<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 	<![endif]-->
 	<div class="logo-back-to-main">
-		<a href="<?=site_url('')?>"><img src="<?=base_url('img/color_logo.png');?>"></a>
+		<a href="<?=site_url('')?>"><img src="<?=base_url('img/white_logo.png');?>"></a>
 	</div>
 	<article class="news-wrapper">
 		<div class="news-header">
