759a7e18391646c3e8790f9d35d386e19470835d
Tumin Artem <at@grapheme.ru>
2013-06-28 18:26:24 +0400

    New nav blocks!

--- application/views/guests_interface/includes/nav-light.php
+++ application/views/guests_interface/includes/nav-light.php
@@ -1,21 +1,21 @@
 <nav class="nav-light" >
-	<h1 class="logo"> </h1>
-	<div class="anchor" data-0="top:201px;" data-2600="top:234px;" data-6400="top:257px;" data-10600="top:283px;" data-13000="top:296px;" data-14500="top:307px;" data-15265="top:-128px;" > </div>
-	<ul class="navigationLinksList">
-		<li class="navigationLink">
-			<a class="navLink-1" href="#top" data-scroll="0">ГЛАВНАЯ</a>
-		</li>
-		<li class="navigationLink">
-			<a class="navLink-2" href="#" data-scroll="2000">НОВОСТИ</a>
-		</li>
-		<li class="navigationLink">
-			<a class="navLink-3" href="#" data-scroll="6400">РЕСТОРАН</a>
-		</li>
-		<li class="navigationLink">
-			<a class="navLink-4" href="#" data-scroll="11400">БАССЕЙН</a>
-		</li>
-		<li class="navigationLink">
-			<a class="navLink-5" href="#" data-scroll="16200">КОНТАКТЫ</a>
-		</li>
-	</ul>
-</nav>
+					<div class="logo"> </div>
+					<div class="anchor" data-0="top:201px;" data-2600="top:234px;" data-6400="top:257px;" data-10600="top:283px;" data-13000="top:296px;" data-14500="top:315px;" data-15400="top:-113px;" > </div>
+					<ul class="navigationLinksList">
+						<li class="navigationLink">
+							<a class="navLink-1" href="#top" data-scroll="0">ГЛАВНАЯ</a>
+						</li>
+						<li class="navigationLink">
+							<a class="navLink-2" href="#" data-scroll="2000">НОВОСТИ</a>
+						</li>
+						<li class="navigationLink">
+							<a class="navLink-3" href="#" data-scroll="6400">РЕСТОРАН</a>
+						</li>
+						<li class="navigationLink">
+							<a class="navLink-4" href="#" data-scroll="11400">БАССЕЙН</a>
+						</li>
+						<li class="navigationLink">
+							<a class="navLink-5" href="#" data-scroll="16200">КОНТАКТЫ</a>
+						</li>
+					</ul>
+				</nav>
