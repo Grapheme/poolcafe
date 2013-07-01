58890feb0170675519b9f60a027450c5e391f3d8
Tumin Artem <at@grapheme.ru>
2013-07-01 13:34:27 +0400

    footer and show all link

--- application/views/guests_interface/includes/footer.php
+++ application/views/guests_interface/includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<div class="container clearfix">
-		<div class="copy">&copy; 2013 Ресторан Pool Cafe St.Tropez</div>
+		<div class="copy">&copy; 2013 Ресторан &nbsp;Pool Cafe St.Tropez</div>
 		<div class="dev">
 			Создание сайта &ndash; <a href="http://grapheme.ru/" target="_blank">«Графема»</a>
 		</div>
