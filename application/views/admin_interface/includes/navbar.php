f2594d2cfdf01558c32776aeed579c780bdd47a9
Andrey S <andrey@grapheme.ru>
2013-10-03 18:55:37 +0400

    admin styles and usability

--- application/views/admin_interface/includes/navbar.php
+++ application/views/admin_interface/includes/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-inverse navbar-fixed-top">
 	<div class="navbar-inner">
-		<div class="container-fluid">
+		<div class="container">
 			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
@@ -9,8 +9,8 @@
 			<a class="brand no-clickable" href="#">Администрирование</a>
 			<div class="nav-collapse collapse">
 				<p class="navbar-text pull-right">
-					Здравствуйте 
-					<a href="#" class="navbar-link">Администратор</a>
+					Здравствуйте, 
+					<a href="#" class="navbar-link">Администратор</a> | 
 					<a href="<?=site_url('log-off');?>">Выход</a>
 				</p>
 			</div>
